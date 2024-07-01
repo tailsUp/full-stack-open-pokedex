0f82cc37510829e9958f3c4548eafa0fe78b12ce
tailsUp <sorsa.git@gmail.com>
2024-07-01 15:18:46 +0300

    Tehtävä 11.9. Push 4.

--- e2e-tests/cypressTesting.cy.js
+++ e2e-tests/cypressTesting.cy.js
@@ -1,8 +1,14 @@
 /* eslint-disable */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:3003')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('link can be accessed', function() {
+    cy.visit('http://localhost:3003')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
