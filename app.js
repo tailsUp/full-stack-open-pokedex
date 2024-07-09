84372bd9d7d2da829cdde5bbf1c9a04c835739f1
tailsUp <sorsa.git@gmail.com>
2024-07-09 21:59:12 +0300

    tehtävä 11.12 healt check sisään. Error muutoksella...2

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/version', (req, res) => {
 })*/
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
+  // eslint-disable-next-line no-constant-condition ASD
   if (true) throw('error...  ')
   res.send('ok')
 })
